7475d595a43c60c5d4bd6a2bc2b80643b2065d3e
Ernest Hwang <Ernest Hwang>
2017-05-11 00:43:29 +0900

    statusbar 색상오류

--- paintee-android/www/js/index/personal.js
+++ paintee-android/www/js/index/personal.js
@@ -38,29 +38,7 @@ function showPersonal(username, paintingId){
     // [tuesday] mainSwiper 순서 하나씩 미룸
     selectMenu(5);
     if(painteeFB.isCordova()){
-        if(StatusBar){
-           if(mainSwiper.activeIndex==0){
-               if(StatusBar){
-                   StatusBar.backgroundColorByHexString("#505050")
-               };
-           }else if(mainSwiper.activeIndex==1){
-               if(StatusBar){
-                   StatusBar.backgroundColorByHexString("#33b3cc")
-               };
-           }else if(mainSwiper.activeIndex==2){
-               if(StatusBar){
-                   StatusBar.backgroundColorByHexString("#cc3380")
-               };
-           }else if(mainSwiper.activeIndex==3){
-               if(StatusBar){
-                   StatusBar.backgroundColorByHexString("#8ab82e")
-               };
-           }else if(mainSwiper.activeIndex==4){
-               if(StatusBar){
                    StatusBar.backgroundColorByHexString("#4c33cc")
-               };
-           }
-        };
     }
 }
 
